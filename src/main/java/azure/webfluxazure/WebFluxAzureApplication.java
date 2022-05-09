d45e73042356a3606d0de72f3c8992a9fc49f00c
matheusgoes23 <matheusgoes23@gmail.com>
2022-05-09 10:49:57 -0300

    Started project

---
+++ src/main/java/azure/webfluxazure/WebFluxAzureApplication.java
@@ -0,0 +1,13 @@
+package azure.webfluxazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azure/webfluxazure/WebFluxAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package azure.webfluxazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
